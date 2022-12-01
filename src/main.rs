3584141daa78aaa5a400e3a3cdcc8b6a6cd7608d
disrupted <hi@salomonpopp.me>
2022-12-02 00:38:57 +0100

    day 1, part 2

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use emergence::AoC;
 fn main() -> Result<(), Box<dyn Error>> {
     let aoc = AoC::new(2022)?;
     let input = aoc.read_or_fetch(1)?;
-    let result = solve(&input).unwrap();
+    let result = solve_part1(&input).unwrap();
     println!("{}:{}", 1 + result.0, result.1);
+
+    let result = solve_part2(&input);
+    println!("{}", result);
     Ok(())
 }
 
-fn solve(input: &str) -> Option<(usize, u32)> {
+fn solve_part1(input: &str) -> Option<(usize, u32)> {
     input
         .split("\n\n") // split on empty lines
         .map(|chunk| {
@@ -21,4 +24,19 @@ fn solve(input: &str) -> Option<(usize, u32)> {
         })
         .enumerate()
         .max_by(|(_, a), (_, b)| a.cmp(b))
+}
+
+fn solve_part2(input: &str) -> u32 {
+    let mut calories = input
+        .split("\n\n") // split on empty lines
+        .map(|chunk| {
+            chunk
+                .lines()
+                .map(|l| l.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+    calories.sort();
+    calories.reverse();
+    calories.iter().take(3).sum()
 }
